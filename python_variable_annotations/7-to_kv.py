479577f3ad55e2787ade2231a39d6ffa0ef00c2e
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-07-28 11:22:52 +0200

    add typing tuple and union

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -2,11 +2,11 @@
 """
 Module 7-to_kv.py
 """
+from typing import Tuple, Union
 
-
-def to_kv(k: str, v: int | float) -> tuple:
+def to_kv(k: str, v: Union[int, float]) -> Tuple:
     """
     Takes a string k and an int OR float v as arguments and returns a tuple.
     """
-    tuple_return: tuple[str, int | float] = (k, v*v)
+    tuple_return: Tuple[str, Union[int, float]] = (k, v*v)
     return tuple_return
