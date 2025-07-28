26e4c98755e7360368e9b76596a819c6f78d2efe
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-07-28 11:19:39 +0200

    change list to typing.list

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -2,9 +2,10 @@
 """
 Module 5-sum_list.py
 """
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """
     Returns the sum of floats from a list of floats
     """
