7f538ee17ab835df93b9e5c4a2c6112359b8c809
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-07-28 11:35:00 +0200

    adding function description

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -2,10 +2,9 @@
 """
 Module 9-element_length.py
 """
-import typing
+from typing import Sequence, Iterable, List, Tuple
 
 
-def element_length(
-    lst: typing.Iterable[typing.Sequence]
-) -> typing.List[typing.Tuple[typing.Sequence, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """Returns a list of tuples containing elements and their lengths."""
     return [(i, len(i)) for i in lst]
