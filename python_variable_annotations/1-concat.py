2234a5c78174a83d3ec9f999e56e65edf6d11fe4
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-07-28 09:57:26 +0200

    task 1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Module 1-concat.py
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenates two strings and return the concatenation
+    """
+    return str1 + str2
