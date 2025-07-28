d6a4990790eca58cb0d5c57cdd8660f7211071b4
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-07-28 10:15:02 +0200

    task 6

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Module 6-sum_mixed_list.py
+"""
+
+
+def sum_mixed_list(mxd_lst: list[float | int]) -> float:
+    """
+    Returns the sum of floats from a list of integers and/or floats
+    """
+    return sum(mxd_lst)
