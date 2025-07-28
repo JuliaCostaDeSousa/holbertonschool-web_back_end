fdfcba1c50a87221f7250b8fd1727880fc4c88cc
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-07-28 10:10:55 +0200

    task 5

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Module 5-sum_list.py
+"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """
+    Returns the sum of floats from a list
+    """
+    return sum(input_list)
