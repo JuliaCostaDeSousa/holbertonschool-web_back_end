0dea33a14c499b2536c757b350e8c5d2db9ed993
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-07-29 09:52:11 +0200

    task0

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,3 +2,14 @@
 """
 Module 0-async_generator.py
 """
+import random
+import asyncio
+
+
+async def async_generator():
+    """
+    Generates a random number 10 times
+    """
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
