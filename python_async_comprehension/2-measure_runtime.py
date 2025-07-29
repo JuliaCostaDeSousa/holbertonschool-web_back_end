b945f68de95adcc29094df1c5c823feaa729fe59
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-07-29 10:25:50 +0200

    task2

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Module 1-async_comprehension.py
+"""
+import time
+import asyncio
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """
+    Execute async_comprehension four times in parallel using asyncio.gather
+    Returns the total runtime
+    """
+    start_time = time.time()
+    tasks = [async_comprehension() for i in range(4)]
+    await asyncio.gather(*tasks)
+    end_time = time.time()
+
+    total_time = end_time - start_time
+    return total_time
