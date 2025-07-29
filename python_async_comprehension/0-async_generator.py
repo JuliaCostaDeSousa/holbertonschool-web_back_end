81986de03bc12ed45feb8c8844c17f1cb572c642
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-07-29 09:55:14 +0200

    adding return type of generator for mypy

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -4,9 +4,10 @@ Module 0-async_generator.py
 """
 import random
 import asyncio
+from typing import AsyncGenerator
 
 
-async def async_generator():
+async def async_generator() -> AsyncGenerator[float, None]:
     """
     Generates a random number 10 times
     """
