640ae2600bb85fdec5ab1838d7b1c1b5448fdf71
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-08-12 11:17:58 +0200

    task 3

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -5,7 +5,7 @@ Deletion-resilient hypermedia pagination
 
 import csv
 import math
-from typing import List
+from typing import List, Dict
 
 
 class Server:
@@ -40,4 +40,24 @@ class Server:
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
-            pass
+        """Dataset with no missing data if some data are deleted
+        """
+        if index is None:
+            index = 0
+        first_index = index
+        max_index = len(self.dataset()) - 1
+        assert 0 <= index <= max_index
+        data = self.indexed_dataset()
+        real_data = []
+        cursor = index
+        while len(real_data) < page_size and cursor <= max_index:
+            if cursor in data:
+                real_data.append(data[cursor])
+            cursor += 1
+        next_index = cursor if cursor <= max_index else None
+        dic = {'index': first_index,
+               'data': real_data,
+               'page_size': len(real_data),
+               'next_index': next_index
+               }
+        return dic
