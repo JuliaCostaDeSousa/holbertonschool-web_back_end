e491309903f718c513a1dc719d964572be673ffd
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-08-14 15:14:02 +0200

    change update many

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -4,6 +4,5 @@
 def update_topics(mongo_collection, name, topics):
     """Changes all topics of a school document based on the name
     """
-    query = {"name": name}
-    new = {"$set":{"topics" :topics}}
-    return mongo_collection.updateMany(query, new)
+    return mongo_collection.update_many({"name": name},
+                                       {"$set":{"topics" :topics}})
