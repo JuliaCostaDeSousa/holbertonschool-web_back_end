00a362d7354385b61209432cf3cff62400f8c987
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-08-26 23:47:07 +0200

    task 7

---
+++ ES6_data_manipulation/7-has_array_values.js
@@ -0,0 +1,11 @@
+export default function hasValuesFromArray(set, array) {
+  if (!Array.isArray(array) || set instanceof Set === 'true') {
+    throw new TypeError();
+  }
+  for (const element of array) {
+    if (!set.has(element)) {
+      return false;
+    }
+  }
+  return true;
+}
