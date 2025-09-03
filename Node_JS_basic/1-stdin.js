447a1c738270a391d232ce5e2062efd658fbb9c0
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-09-03 11:52:34 +0200

    pause instead of exit

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,12 +1,12 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
-process.stdin.setEncoding('utf8');
+
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
   if (name) {
     process.stdout.write(`Your name is: ${name}\n`);
   }
   if (process.stdin.isTTY) {
-    process.exit(0);
+    process.stdin.pause();
   }
 });
 process.stdin.on('end', () => {
