5a5684c8b1cb3c17487f5698767312174ef11a44
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-09-03 12:32:59 +0200

    remove to much check

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,6 +1,5 @@
 function countStudents(path) {
   const fs = require('fs');
-  const fields = [];
   let data;
 
   try {
@@ -8,34 +7,22 @@ function countStudents(path) {
   } catch (err) {
     throw new Error('Cannot load the database');
   }
-  const lines = data.split(/\r?\n/);
-  if (lines.length === 0) {
-    process.stdout.write('Number of students: 0\n');
-    return;
-  }
-  const headerLine = lines[0].split(',').map(h => h.trim());
+  const lines = data.trim().split('\n');
+  const headerLine = lines[0].split(',');
   const idxFirstname = headerLine.indexOf('firstname');
   const idxField = headerLine.indexOf('field');
 
-  if (idxField < 0 || idxFirstname < 0) {
-   process.stdout.write('Number of students: 0\n');
-   return;
-  }
-
-  let linesCleaned = [];
+  const linesCleaned = [];
+  const fields = [];
 
   for (let i = 1; i < lines.length; i += 1) {
-    const element = lines[i];
-    if (!element || element.trim() === '') continue;
-    const splitLine = element.split(',').map(s => s.trim());
-    if (splitLine.length !== headerLine.length) continue;
-
-    if (splitLine[idxFirstname] !== '' && splitLine[idxField] !== '') {
-      linesCleaned.push(splitLine);
-      fields.push(splitLine[idxField].trim());
+    const splitLine = lines[i].split(',');
+    linesCleaned.push(splitLine);
+    fields.push(splitLine[idxField].trim());
     }
-  }
+
   process.stdout.write(`Number of students: ${linesCleaned.length}\n`);
+  
   const uniqueFieldsArr = Array.from(new Set(fields));
 
   for (let i = 0; i < uniqueFieldsArr.length; i += 1) {
