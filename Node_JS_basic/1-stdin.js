27a58c19fcc4105c55f338934266247e8d4052b8
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-09-03 11:48:06 +0200

    add interactive mode working

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -4,6 +4,9 @@ process.stdin.on('data', (data) => {
   if (name) {
     process.stdout.write(`Your name is: ${name}\n`);
   }
+  if (process.stdin.isTTY) {
+    process.exit(0);
+  }
 });
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
