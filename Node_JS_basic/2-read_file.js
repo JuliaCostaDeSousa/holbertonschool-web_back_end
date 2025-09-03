5acb9f3205d12bcbb9dfddc375b34df456e9fecf
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-09-03 12:37:45 +0200

    check first if database exists

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,35 +1,36 @@
 function countStudents(path) {
   const fs = require('fs');
-  let data;
 
-  if (!fs.existsSync(database)) {
+  try {
+    fs.existsSync(path);
+  } catch (err) {
     throw new Error('Cannot load the database');
-  } else {
-    const lines = data.trim().split('\n');
-    const headerLine = lines[0].split(',');
-    const idxFirstname = headerLine.indexOf('firstname');
-    const idxField = headerLine.indexOf('field');
+  }
+  const data = fs.readFileSync(database, 'utf8');
+  const lines = data.trim().split('\n');
+  const headerLine = lines[0].split(',');
+  const idxFirstname = headerLine.indexOf('firstname');
+  const idxField = headerLine.indexOf('field');
 
-    const linesCleaned = [];
-    const fields = [];
+  const linesCleaned = [];
+  const fields = [];
 
-    for (let i = 1; i < lines.length; i += 1) {
-      const splitLine = lines[i].split(',');
-      linesCleaned.push(splitLine);
-      fields.push(splitLine[idxField].trim());
-      }
-
-    process.stdout.write(`Number of students: ${linesCleaned.length}\n`);
-
-    const uniqueFieldsArr = Array.from(new Set(fields));
-
-    for (let i = 0; i < uniqueFieldsArr.length; i += 1) {
-      const Ufield = uniqueFieldsArr[i];
-      const selection = linesCleaned.filter(f => f[idxField] === Ufield);
-      const students = selection.map(col => col[idxFirstname]);
-      process.stdout.write(
-        `Number of students in ${Ufield}: ${selection.length}. List: ${students.join(', ')}\n`);
+  for (let i = 1; i < lines.length; i += 1) {
+    const splitLine = lines[i].split(',');
+    linesCleaned.push(splitLine);
+    fields.push(splitLine[idxField].trim());
     }
-  }  
+
+  process.stdout.write(`Number of students: ${linesCleaned.length}\n`);
+  
+  const uniqueFieldsArr = Array.from(new Set(fields));
+
+  for (let i = 0; i < uniqueFieldsArr.length; i += 1) {
+    const Ufield = uniqueFieldsArr[i];
+    const selection = linesCleaned.filter(f => f[idxField] === Ufield);
+    const students = selection.map(col => col[idxFirstname]);
+    process.stdout.write(
+        `Number of students in ${Ufield}: ${selection.length}. List: ${students.join(', ')}\n`);
+  }
 }
 module.exports = countStudents;
